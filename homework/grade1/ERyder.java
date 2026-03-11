e81d860e0e36ebd93f05e0ae36b39b248f88a7ce
Lzz <3168144566@qq.com>
2026-03-11 15:39:37 +0800

    this is homework

--- homework/grade1/ERyder.java
+++ homework/grade1/ERyder.java
@@ -1,15 +1,32 @@
 package homework.grade1;
 
 public class ERyder {
-    
+
+    public static final String COMPANY_NAME = "ERyder";
+    public static final double BASE_FARE = 1.0;
+    public static final double PER_MINUTE_FARE = 0.5;
+
+    private final String LINKED_ACCOUNT;
+    private final String LINKED_PHONE_NUMBER;
 
     private String bikeID;
     private int batteryLevel;
     private boolean isAvailable;
     private double kmDriven;
 
+    private int totalUsageInMinutes;
+    private double totalFare;
+
 
     public ERyder() {
+        this.bikeID = "";
+        this.batteryLevel = 0;
+        this.isAvailable = false;
+        this.kmDriven = 0.0;
+        this.LINKED_ACCOUNT = "";
+        this.LINKED_PHONE_NUMBER = "";
+        this.totalUsageInMinutes = 0;
+        this.totalFare = 0.0;
     }
 
 
@@ -18,6 +35,23 @@ public class ERyder {
         this.batteryLevel = batteryLevel;
         this.isAvailable = isAvailable;
         this.kmDriven = kmDriven;
+        this.LINKED_ACCOUNT = "default_user";
+        this.LINKED_PHONE_NUMBER = "00000000000";
+        this.totalUsageInMinutes = 0;
+        this.totalFare = 0.0;
+    }
+
+
+    public ERyder(String bikeID, int batteryLevel, boolean isAvailable, double kmDriven, 
+                  String linkedAccount, String linkedPhoneNumber) {
+        this.bikeID = bikeID;
+        this.batteryLevel = batteryLevel;
+        this.isAvailable = isAvailable;
+        this.kmDriven = kmDriven;
+        this.LINKED_ACCOUNT = linkedAccount;
+        this.LINKED_PHONE_NUMBER = linkedPhoneNumber;
+        this.totalUsageInMinutes = 0;
+        this.totalFare = 0.0;
     }
 
 
@@ -38,6 +72,24 @@ public class ERyder {
         System.out.println("行驶里程：" + kmDriven + " 千米");
     }
 
+
+    public void printRideDetails(int usageInMinutes) {
+        this.totalUsageInMinutes = usageInMinutes;
+        this.totalFare = calculateFare(usageInMinutes);
+        
+        System.out.println("=== 骑行详情 ===");
+        System.out.println("公司名称：" + COMPANY_NAME);
+        System.out.println("关联账户：" + LINKED_ACCOUNT);
+        System.out.println("关联手机号：" + LINKED_PHONE_NUMBER);
+        System.out.println("车辆编号：" + bikeID);
+        System.out.println("使用时长：" + usageInMinutes + " 分钟");
+        System.out.println("总费用：" + totalFare + " 元");
+    }
+
+
+    private double calculateFare(int usageInMinutes) {
+        return BASE_FARE + (PER_MINUTE_FARE * usageInMinutes);
+    }
 
 
     public String getBikeID() {
--- homework/grade1/Run.java
+++ homework/grade1/Run.java
@@ -5,8 +5,14 @@ public class Run {
         ERyder bike1 = new ERyder("B001", 55, true, 25.3);
         bike1.printBikeDetails();
         bike1.ride();
-        ERyder bike2= new ERyder();
+
+        ERyder bike2 = new ERyder("B002", 80, true, 10.5, "zhangsan", "13800138000");
         bike2.printBikeDetails();
         bike2.ride();
+
+        System.out.println("\n=== 骑行详情展示 ===");
+        bike1.printRideDetails(30);
+        System.out.println();
+        bike2.printRideDetails(45);
     }
 }
